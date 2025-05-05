0ae2aa66e5397ad06a6a893443d15d0930c17bd1
hyt2023009 <3247547842@qq.com>
2025-05-05 23:49:18 +0800

    fibo

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdlib.h> 
 #include <stdio.h> 
-#include "fibo.h" 
-   
-int main (int argc, char * argv []) 
-{ 
-	long ret; 
+#include “fibo.h” 
+	
+int main (int argc, char * argv []) { 
+       	long ret; 
 	int n; 
-	if (argc<2){ 
-		fprintf (stderr, "missing index.\n"); 
-		return -1; 
-	} 
+       	if (argc<2){ 
+	   	printf (stderr, “missing index.\n”);
+	  	return -1; 
+       	} 
 	n=atoi (argv [1]); 
 	ret = fibo (n); 
-	printf ("Fibo (%d) is %ld\n",n,ret); 
-	return EXIT_SUCCESS; 
-}
+     	printf (“fibo (%d) is %ld\n”,n,ret); 
+      	return EXIT_SUCCESS;
+} 
 
